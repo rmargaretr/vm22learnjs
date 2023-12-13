d3d0521ceebeef85f01f0c5582f401c5f8996ff4
rmargaretr <margaret.rahu@tptlive.ee>
2023-12-13 16:13:21 +0200

    tehtud

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-console.log('hello world!');
+const express = require('express')
+const app = express()
+const port = 8080
+const path = require('path');
 
-let num = 1;
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+})
 
-//var text = 'Hello': Dont use var
-console.log(num);
-let string = 'hello';
+app.get("/calculate", (req, res) => {
+    const [a, b] = [req.query.a, req.query.b];
+    console.log(a,b);
+    if (a == null || b == null) return res.json({"answer": null});
+    let hypotenuse = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
+    res.json({"answer": hypotenuse})
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
